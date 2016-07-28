50f23cc0e0d256abc2306860d3e22bd1faf13e57
mingchun.or <mingchuno@gmail.com>
2016-07-28 11:39:18 +0800

    add gcsql support

--- pogom/utils.py
+++ pogom/utils.py
@@ -100,6 +100,8 @@ def get_args():
     parser.add_argument('--db-user', help='Username for the database')
     parser.add_argument('--db-pass', help='Password for the database')
     parser.add_argument('--db-host', help='IP or hostname for the database')
+    parser.add_argument('--db-instance', help='Google Cloud SQL DB instance name')
+
     parser.add_argument('-wh', '--webhook', help='Define URL(s) to POST webhook information to',
                         nargs='*', default=False, dest='webhooks')
     parser.set_defaults(DEBUG=False)
